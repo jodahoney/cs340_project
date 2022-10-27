3e61a9d451500dde6c079fcfae7c4c0f7fc36e41
Joseph <jdehoney@stanford.edu>
2022-10-26 21:59:07 -0700

    added base template

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+# Airports
+
+# Airplanes
+
+# Flights
+
+# Pilots
+
+# Customers
