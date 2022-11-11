afe65faa92c0d432b1f41fbed11bc83e0e342b97
Joseph Dehoney <josephdehoney@gmail.com>
2022-11-10 21:36:11 -0800

    Implemnetation is done for all except edit

--- test_proj/app.py
+++ test_proj/app.py
@@ -28,18 +28,18 @@ def orcas():
 @app.route("/airports", methods=["POST", "GET"])
 def airports():
 
-    # insert a person into the airports entity
+    # insert into the airports entity
     if request.method == "POST":
         # fire off if user presses the Add Person button
         if request.form.get("Add_Airport"):
             # grab user form inputs
-            airportID = request.form["airportID"]
-            name = request.form["name"]
-            city = request.form["city"]
-            state = request.form["state"]
+            airportID = request.form["AirportID"]
+            name = request.form["Name"]
+            city = request.form["City"]
+            state = request.form["State"]
         
             # assuming no null inputs
-            query = "INSERT INTO Airports (airportID, name, city, state) VALUES (%s, %s,%s,%s);"
+            query = "INSERT INTO Airports (AirportID, Name, City, State) VALUES (%s, %s,%s,%s);"
             # cur = db.connection.cursor()
             # cur.execute(query, (airportID, name, city, state))
             # db.connection.commit()
@@ -78,7 +78,7 @@ def delete_airport(id):
 def edit_airport(id):
     if request.method == "GET":
         # mySQL query to grab the info of the person with our passed id
-        query = "SELECT * FROM Airports WHERE id = %s" % (id)
+        query = "SELECT * FROM Airports WHERE AirportID = '%s';" % (id)
         cursor = db.execute_query(db_connection=db_connection, query=query)
         data = cursor.fetchall()
 
@@ -86,21 +86,19 @@ def edit_airport(id):
 
     # meat and potatoes of our update functionality
     if request.method == "POST":
-        # fire off if user clicks the 'Edit airport' button
         if request.form.get("Edit_Airport"):
             # grab user form inputs
-            airportID = request.form["airportID"]
-            name = request.form["name"]
-            city = request.form["city"]
-            state = request.form["state"]
+            airportID = request.form["AirportID"]
+            name = request.form["Name"]
+            city = request.form["City"]
+            state = request.form["State"]
 
-            query = "UPDATE Airports SET Airports.name = %s, Airports.city = %s, Airports.state = %s WHERE Airports.airportID = %s"
-            cur = db.connection.cursor()
-            cur.execute(query, (name, city, state, airportID))
-            db.connection.commit()
+            query = "UPDATE Airports SET Airports.Name = %s, Airports.City = %s, Airports.State = %s WHERE Airports.AirportID = %s;"
+            cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(name, city, state, airportID))
 
             # redirect back to people page after we execute the update query
-            return redirect("/people")
+            return redirect("/airports")
+
 
             
 
