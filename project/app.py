a37646400ce90c9c11cea144c777d3b8ea458bd0
Joseph Dehoney <josephdehoney@gmail.com>
2022-12-04 21:57:24 -0800

    Added update to Flights

--- project/app.py
+++ project/app.py
@@ -8,6 +8,20 @@ db_connection = db.connect_to_database()
 
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
+def calculate_flight_time(arrival, departure):
+    """
+    Accepts the string output by the HTML datetime-local input form and converts
+    to a time delta in number of minutes from departure to arrival.
+    """
+    arrival_datetime = datetime.datetime.strptime(arrival,"%Y-%m-%dT%H:%M")
+    departure_datetime = datetime.datetime.strptime(departure,"%Y-%m-%dT%H:%M")
+
+    time_delta = arrival_datetime - departure_datetime
+    FlightDuration = (time_delta.days * 24) + (time_delta.seconds // 60)
+    
+    return FlightDuration
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,7 +31,7 @@ def airports():
 
     # insert into the airports entity
     if request.method == "POST":
-        # fire off if user presses the Add Person button
+        
         if request.form.get("Add_Airport"):
             # grab user form inputs
             airportID = request.form["AirportID"]
@@ -27,9 +41,6 @@ def airports():
         
             # assuming no null inputs
             query = "INSERT INTO Airports (`AirportID`, `Name`, `City`, `State`) VALUES (%s, %s, %s, %s);"
-            # cur = db.connection.cursor()
-            # cur.execute(query, (airportID, name, city, state))
-            # db.connection.commit()
             cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(airportID, name, city, state))
             results = cursor.fetchall()
 
@@ -37,17 +48,14 @@ def airports():
 
     # Grab Airports data so we send it to our template to display
     if request.method == "GET":
-        # db query to grab all the people in bsg_people
         query = "SELECT * FROM Airports;"
         cursor = db.execute_query(db_connection=db_connection, query=query)
         data = cursor.fetchall()
 
-        # render edit_people page passing our query data and homeworld data to the edit_people template
         return render_template("airports.html", data=data)
 
 @app.route("/delete-airport/<string:id>")
 def delete_airport(id):
-    # mySQL query to delete the person with our passed id
     query = "DELETE FROM Airports WHERE AirportID = '%s';" % (id)
     cursor = db.execute_query(db_connection=db_connection, query=query)
     data = cursor.fetchall()
@@ -58,34 +66,30 @@ def delete_airport(id):
 @app.route("/edit-airport/<string:id>", methods=["POST", "GET"])
 def edit_airport(id):
     if request.method == "GET":
-        # mySQL query to grab the info of the person with our passed id
         query = "SELECT * FROM Airports WHERE AirportID = '%s';" % (id)
         cursor = db.execute_query(db_connection=db_connection, query=query)
         data = cursor.fetchall()
 
         return render_template("edit_airport.html", data=data)
 
-    # meat and potatoes of our update functionality
     if request.method == "POST":
         if request.form.get("Edit_Airport"):
-            # grab user form inputs
             name = request.form["Name"]
             city = request.form["City"]
             state = request.form["State"]
             
-            # . UPDATE table_name SET column1 = value1, column2 = value2 WHERE id=100;
             query = "UPDATE Airports SET Airports.Name = %s, Airports.City = %s, Airports.State = %s WHERE Airports.AirportID = %s;"
             cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(name, city, state, id))
             data = cursor.fetchall()
             print(data)
-            # redirect back to people page after we execute the update query
+            
             return redirect("/airports")
 
 # Airplanes
 @app.route("/airplanes", methods=["POST", "GET"])
 def airplanes():
 
-    # insert into the airports entity
+    # insert into the airplanes entity
     if request.method == "POST":
         # fire off if user presses the Add airplane button
         if request.form.get("Add_Airplane"):
@@ -100,51 +104,41 @@ def airplanes():
         
             # assuming no null inputs
             query = "INSERT INTO Airplanes (`TailNumber`, `Make`, `Model`, `Range`, `FuelCapacity`, `LastMaintenancePerformed`, `MaintenanceFrequency`) VALUES (%s, %s, %s, %s, %s, %s, %s);"
-            # cur = db.connection.cursor()
-            # cur.execute(query, (airportID, name, city, state))
-            # db.connection.commit()
             cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(tail_number, make, model, air_range, fuel_capacity, last_maintenance_performed, maintenance_frequency))
             results = cursor.fetchall()
 
             return redirect("/airplanes")
 
-    # Grab Airports data so we send it to our template to display
+    # Grab Airplanes data so we send it to our template to display
     if request.method == "GET":
-        # db query to grab all the people in bsg_people
         query = "SELECT * FROM Airplanes;"
         cursor = db.execute_query(db_connection=db_connection, query=query)
         data = cursor.fetchall()
 
-        # render edit_people page passing our query data and homeworld data to the edit_people template
         return render_template("airplanes.html", data=data)
 
 
 # route for delete functionality, deleting an airplane
-# we want to pass the 'id' value of that person on button click (see HTML) via the route
 @app.route("/delete-airplane/<string:id>")
 def delete_airplane(id):
-    # mySQL query to delete the person with our passed id
     query = "DELETE FROM Airplanes WHERE TailNumber = '%s';" % (id)
     cursor = db.execute_query(db_connection=db_connection, query=query)
     data = cursor.fetchall()
 
-    # redirect back to airport page
+    # redirect back to airplanes page
     return redirect("/airplanes")
 
 
-# route for edit functionality, updating the attributes of a person in bsg_people
-# similar to our delete route, we want to the pass the 'id' value of that person on button click (see HTML) via the route
+# route for edit functionality, updating the attributes of an airplane
 @app.route("/edit-airplane/<string:id>", methods=["POST", "GET"])
 def edit_airplane(id):
     if request.method == "GET":
-        # mySQL query to grab the info of the person with our passed id
         query = "SELECT * FROM Airplanes WHERE TailNumber = '%s';" % (id)
         cursor = db.execute_query(db_connection=db_connection, query=query)
         data = cursor.fetchall()
 
         return render_template("edit_airplane.html", data=data)
 
-    # meat and potatoes of our update functionality
     if request.method == "POST":
         if request.form.get("Edit_Airplane"):
             make = request.form["Make"]
@@ -153,13 +147,12 @@ def edit_airplane(id):
             fuel_capacity = request.form["FuelCapacity"]
             last_maintenance_performed = request.form["LastMaintenancePerformed"]
             maintenance_frequency = request.form["MaintenanceFrequency"]
-            
-            # . UPDATE table_name SET column1 = value1, column2 = value2 WHERE id=100;
+           
             query = "UPDATE Airplanes SET Airplanes.Make = %s, Airplanes.Model = %s, Airplanes.Range = %s, Airplanes.FuelCapacity = %s, Airplanes.LastMaintenancePerformed = %s, Airplanes.MaintenanceFrequency = %s WHERE Airplanes.TailNumber = %s;"
             cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(make, model, range, fuel_capacity, last_maintenance_performed, maintenance_frequency, id))
             data = cursor.fetchall()
             print(data)
-            # redirect back to people page after we execute the update query
+ 
             return redirect("/airplanes")
             
 # Flights
@@ -191,11 +184,7 @@ def flights():
 
             
             # Calculate travel time
-            arrival_datetime = datetime.datetime.strptime(Arrival,"%Y-%m-%dT%H:%M")
-            departure_datetime = datetime.datetime.strptime(Departure,"%Y-%m-%dT%H:%M")
-
-            time_delta = arrival_datetime - departure_datetime
-            FlightDuration = (time_delta.days * 24) + (time_delta.seconds // 60)
+            FlightDuration = calculate_flight_time(Arrival, Departure)
         
             # assuming no null inputs
             query = "INSERT INTO Flights \
@@ -225,17 +214,8 @@ def flights():
                     )
             return redirect("/flights")
 
-    # Grab Flights data so we send it to our template to display
+    # Grab Flights and all related data so we send it to our template to display
     if request.method == "GET":
-        # OLD WAY OF SELECTING CUSTOMERS INTO TABLE:
-        # query = "SELECT \
-        #     FlightID, Origin, Destination, Departure, Arrival, FlightDuration, Pilot, Copilot, Aircraft, \
-        #     Customers.FirstName AS fname, Customers.LastName AS lname   \
-        #     FROM Flights \
-        #     INNER JOIN Flights_has_Customers \
-        #     ON Flights.FlightID = Flights_has_Customers.Flights_FlightID \
-        #     LEFT JOIN Customers \
-        #     ON Flights_has_Customers.Customers_CustomerID = Customers.CustomerID;"
 
         query = "SELECT * FROM Flights;"
         cursor = db.execute_query(db_connection=db_connection, query=query)
@@ -257,13 +237,13 @@ def flights():
         cursor = db.execute_query(db_connection=db_connection, query=query5)
         aircraft_data = cursor.fetchall()
 
-        # render edit_people page passing our query data and homeworld data to the edit_people template
         return render_template("flights.html", 
             data=data, 
             customer_data=customer_data, 
             airport_data=airport_data,
             pilot_data=pilot_data,
-            aircraft_data=aircraft_data)
+            aircraft_data=aircraft_data
+            )
 
 
 # route for delete functionality, deleting a flight
@@ -283,38 +263,63 @@ def delete_flight(id):
     return redirect("/flights")
 
 
-# route for edit functionality, updating the attributes of a person in bsg_people
-# similar to our delete route, we want to the pass the 'id' value of that person on button click (see HTML) via the route
 @app.route("/edit-flight/<int:id>", methods=["POST", "GET"])
 def edit_flight(id):
     if request.method == "GET":
-        # mySQL query to grab the info of the person with our passed id
+        
         query = "SELECT * FROM Flights WHERE FlightID = '%s';" % (id)
+
+        query3 = "SELECT AirportID, Name FROM Airports;"
+        cursor = db.execute_query(db_connection=db_connection, query=query3)
+        airport_data = cursor.fetchall()
+
+        query4 = "SELECT PilotID, FirstName, LastName FROM Pilots;"
+        cursor = db.execute_query(db_connection=db_connection, query=query4)
+        pilot_data = cursor.fetchall()
+
+        query5 = "Select TailNumber, Make, Model FROM Airplanes;"
+        cursor = db.execute_query(db_connection=db_connection, query=query5)
+        aircraft_data = cursor.fetchall()
+
         cursor = db.execute_query(db_connection=db_connection, query=query)
         data = cursor.fetchall()
 
-        return render_template("edit_flight.html", data=data)
+        return render_template("edit_flight.html", 
+            data=data, 
+            # customer_data=customer_data, 
+            airport_data=airport_data,
+            pilot_data=pilot_data,
+            aircraft_data=aircraft_data)
 
-    # meat and potatoes of our update functionality
     if request.method == "POST":
         if request.form.get("Edit_Flight"):
+            
             # grab user form inputs
-            name = request.form[""]
-            city = request.form[""]
-            state = request.form[""]
+            Origin = request.form["origin-select"]
+            Destination = request.form["destination-select"]
+            Departure = request.form["Departure"]
+            Arrival = request.form["Arrival"]
+            Pilot = request.form["pilot-select"]
+            CoPilot = request.form["copilot-select"]
+            Aircraft = request.form["aircraft-select"]
+
+            FlightDuration = calculate_flight_time(Arrival, Departure)
         
-            query = "UPDATE Flights SET Airports.Name = %s, Airports.City = %s, Airports.State = %s WHERE Airports.AirportID = %s;"
-            cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(name, city, state, id))
+            query = "UPDATE Flights SET Flights.Origin = %s, Flights.Destination = %s, Flights.Departure = %s, \
+                Flights.Arrival = %s, Flights.FlightDuration = %s, Flights.Pilot = %s, Flights.CoPilot = %s, Flights.Aircraft = %s \
+                WHERE Flights.FlightID = %s;"
+            cursor = db.execute_query(db_connection=db_connection,
+                query=query,
+                query_params=(Origin, Destination, Departure, Arrival, FlightDuration, Pilot, CoPilot, Aircraft, id)
+            )
             data = cursor.fetchall()
-            print(data)
-            # redirect back to people page after we execute the update query
+            
             return redirect("/flights")
 
 # Pilots
 @app.route("/pilots", methods=["POST", "GET"])
 def pilots():
 
-    # insert into the airports entity
     if request.method == "POST":
         # fire off if user presses the Add Person button
         if request.form.get("Add_Pilot"):
@@ -330,19 +335,14 @@ def pilots():
 
             return redirect("/pilots")
 
-    # Grab Airports data so we send it to our template to display
     if request.method == "GET":
-        # db query to grab all the people in bsg_people
         query = "SELECT * FROM Pilots;"
         cursor = db.execute_query(db_connection=db_connection, query=query)
         data = cursor.fetchall()
 
-        # render edit_people page passing our query data and homeworld data to the edit_people template
         return render_template("pilots.html", data=data)
 
 
-# route for delete functionality, deleting an airport
-# we want to pass the 'id' value of that person on button click (see HTML) via the route
 @app.route("/delete-pilot/<string:id>")
 def delete_pilot(id):
     # mySQL query to delete the person with our passed id
@@ -350,23 +350,17 @@ def delete_pilot(id):
     cursor = db.execute_query(db_connection=db_connection, query=query)
     data = cursor.fetchall()
 
-    # redirect back to airport page
     return redirect("/pilots")
 
-
-# route for edit functionality, updating the attributes of a person in bsg_people
-# similar to our delete route, we want to the pass the 'id' value of that person on button click (see HTML) via the route
 @app.route("/edit-pilot/<string:id>", methods=["POST", "GET"])
 def edit_pilot(id):
     if request.method == "GET":
-        # mySQL query to grab the info of the person with our passed id
         query = "SELECT * FROM Pilots WHERE PilotID = '%s';" % (id)
         cursor = db.execute_query(db_connection=db_connection, query=query)
         data = cursor.fetchall()
 
         return render_template("edit_pilot.html", data=data)
 
-    # meat and potatoes of our update functionality
     if request.method == "POST":
         if request.form.get("Edit_Pilot"):
             # grab user form inputs
@@ -374,12 +368,11 @@ def edit_pilot(id):
             lname = request.form["LastName"]
             phone_number = request.form["PhoneNumber"]
             
-            # . UPDATE table_name SET column1 = value1, column2 = value2 WHERE id=100;
             query = "UPDATE Pilots SET Pilots.FirstName = %s, Pilots.LastName = %s, Pilots.PhoneNumber = %s WHERE Pilots.PilotID = %s;"
             cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(fname, lname, phone_number, id))
             data = cursor.fetchall()
             print(data)
-            # redirect back to people page after we execute the update query
+            
             return redirect("/pilots")
 
 # Customers
@@ -409,41 +402,30 @@ def customers():
 
     # Grab Customers data so we send it to our template to display
     if request.method == "GET":
-        # db query to grab all the people in bsg_people
         query = "SELECT * FROM Customers;"
         cursor = db.execute_query(db_connection=db_connection, query=query)
         data = cursor.fetchall()
 
-        # render edit_people page passing our query data and homeworld data to the edit_people template
         return render_template("customers.html", data=data)
 
-
-# route for delete functionality, deleting an airport
-# we want to pass the 'id' value of that person on button click (see HTML) via the route
 @app.route("/delete-customer/<string:id>")
 def delete_customer(id):
-    # mySQL query to delete the person with our passed id
     query = "DELETE FROM Customers WHERE CustomerID = '%s';" % (id)
     cursor = db.execute_query(db_connection=db_connection, query=query)
     data = cursor.fetchall()
 
-    # redirect back to airport page
     return redirect("/customers")
 
 
-# route for edit functionality, updating the attributes of a person in bsg_people
-# similar to our delete route, we want to the pass the 'id' value of that person on button click (see HTML) via the route
 @app.route("/edit-customer/<string:id>", methods=["POST", "GET"])
 def edit_customer(id):
     if request.method == "GET":
-        # mySQL query to grab the info of the person with our passed id
         query = "SELECT * FROM Customers WHERE CustomerID = '%s';" % (id)
         cursor = db.execute_query(db_connection=db_connection, query=query)
         data = cursor.fetchall()
 
         return render_template("edit_customer.html", data=data)
 
-    # meat and potatoes of our update functionality
     if request.method == "POST":
         if request.form.get("Edit_Customer"):
             # grab user form inputs
@@ -456,12 +438,11 @@ def edit_customer(id):
             zip_code = request.form["ZIP_Code"]
             phone_number = request.form["PhoneNumber"]
             
-            # . UPDATE table_name SET column1 = value1, column2 = value2 WHERE id=100;
             query = "UPDATE Customers SET Customers.FirstName = %s, Customers.LastName = %s, Customers.AddressLine1 = %s, Customers.AddressLine2 = %s, Customers.City = %s, Customers.State = %s, Customers.ZIP_Code = %s, Customers.PhoneNumber = %s WHERE Customers.CustomerID = %s;"
             cursor = db.execute_query(db_connection=db_connection, query=query, query_params=(fname, lname, address1, address2, city, state, zip_code, phone_number, id))
             data = cursor.fetchall()
             print(data)
-            # redirect back to people page after we execute the update query
+            
             return redirect("/customers")
 
 # Flights_has_customers
@@ -481,7 +462,6 @@ def flightsCustomers():
         cursor = db.execute_query(db_connection=db_connection, query=query2)
         customer_data = cursor.fetchall()
 
-        # render edit_people page passing our query data and homeworld data to the edit_people template
         return render_template("flights_has_customers.html", data=data, customer_data=customer_data)
     
     return render_template('flights_has_customers.html')
